ee57b94b4c608490c37ae5b2bf038d6486346d5b
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-05-31 04:07:37 +0500

    Add constants demo using final keyword
    Shows how to declare and use constants in Java using the final keyword. Constants cannot be changed once assigned.

---
+++ 01_Basics/ConstantsDemo.java
@@ -0,0 +1,11 @@
+class ConstantsDemo {
+    public static void main(String[] args) {
+        final int MAX_SPEED = 120;
+        final double PI = 3.14159;
+        final String COUNTRY = "Pakistan";
+
+        System.out.println("Max Speed: " + MAX_SPEED);
+        System.out.println("PI: " + PI);
+        System.out.println("Country: " + COUNTRY);
+    }
+}
