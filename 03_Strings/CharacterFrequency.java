d8069179f787aa18050cace4d3b6cd711aadc29f
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-06-05 10:30:55 +0500

    Create CharacterFrequency.java

---
+++ 03_Strings/CharacterFrequency.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class CharacterFrequency {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String sentence = input.nextLine();
+        System.out.print("Enter a character to find: ");
+        char toFind = input.nextLine().charAt(0);
+
+        int count = 0;
+        for (int i = 0; i < sentence.length(); i++) {
+            if (sentence.charAt(i) == toFind) {
+                count++;
+            }
+        }
+        System.out.print("'" + toFind + "' appears " + count + " time(s)");
+    }
+}
